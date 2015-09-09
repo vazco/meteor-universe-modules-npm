f771cf6415492ead68ff5bb28d397e92f7131828
cristo-rabani <cristo.rabani@gmail.com>
2015-09-09 02:21:32 +0200

    add

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'universe:modules-npm',
-    version: '0.0.1',
+    version: '0.5.0',
     // Brief, one-line summary of the package.
     summary: 'Import of npm packages that works on client and server',
     // URL to the Git repository containing the source code for this package.
